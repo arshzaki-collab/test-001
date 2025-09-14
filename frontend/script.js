56d91788e097211f077eda26b48153cd16cd758d
arsh zaki <arsh.zaki@gmail.com>
2025-09-14 12:39:43 +0530

    console log added with modified language selection

--- frontend/script.js
+++ frontend/script.js
@@ -320,6 +320,7 @@ document.addEventListener('DOMContentLoaded', () => {
             localStorage.setItem('krishi_lang_chosen', 'true');
             applyLanguage(currentLang);
             hideLanguageModal();
+            console.log("--current langauge clicked--->", currentLang)
         });
     });
 
